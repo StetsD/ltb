aca2cb00753595c6b5813fe3c2f11c60e5568261
StetsD <chortstd@gmail.com>
2018-05-16 17:38:58 +0300

    res

--- app/Http/Controllers/Admin/AboutController.php
+++ app/Http/Controllers/Admin/AboutController.php
@@ -4,10 +4,21 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Response;
 
 class AboutController extends Controller
 {
     public function show(){
-		return view('default.about')->withTitle('Maza');
+		$view = view('default.about')->withTitle('Maza')->render();
+		return (new Response($view))
+									->header('Header-One', 'Lalala')
+									->header('Header-Two', 'lololo');
+
+		// return redirect('/');
+
+		// return response()->view('default.about', ['title'=>'Hello world']);
+		// return response()->download('robots.txt', 'mytext.txt');
+
+		// return view('default.about')->withTitle('Maza');
 	}
 }
